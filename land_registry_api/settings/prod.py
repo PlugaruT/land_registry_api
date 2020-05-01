0456292a50b5b2145b23063419db47521922d78c
PlugaruT <plugaru.tudor@protonmail.com>
2020-05-01 11:31:03 +0300

    Debug in prod

--- land_registry_api/settings/prod.py
+++ land_registry_api/settings/prod.py
@@ -4,7 +4,7 @@ import dj_database_url
 # SECRET_KEY = os.environ.get("SECRET_KEY")
 
 ENVIRONMENT = 'prod'
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 DATABASES['default'] = dj_database_url.config(
     default='postgres://cnzgrcninaokmr:a8c1d78511f743b041fe30a7c4702903cb40a7df3c62bfe8406fec1a3c7e86db@ec2-54-247-78-30.eu-west-1.compute.amazonaws.com:5432/d9jaiqletq5gf3'
